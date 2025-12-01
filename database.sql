6f1870b0baef7a0bb4fa5f7cd3a5757c5a9671e5
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-12-01 18:24:29 -0500

    :sparkles: feat:(database): add AUTO_INCREMENT to ids

--- database.sql
+++ database.sql
@@ -5,16 +5,17 @@ USE pizzeria_don_piccoro; -- Cambiar a la base de datos
 -- Crear tabla persona
 
 CREATE TABLE persona (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     telefono VARCHAR(50) NOT NULL,
-    correo_electronico VARCHAR(50) NOT NULL
+    correo_electronico VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- Crear tabla usuario
 
 CREATE TABLE usuario{
-    id INT
+    id INT NOT NULL AUTO_INCREMENT
     usuario VARCHAR(50) NOT NULL,
     clave VARCHAR(50) NOT NULL,
     PRIMARY KEY (id),
@@ -24,45 +25,50 @@ CREATE TABLE usuario{
 -- Crear tabla cliente
 
 CREATE TABLE cliente (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES persona(id)
 );
 
 -- Crear tabla pizza
 
 CREATE TABLE pizza (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     tam VARCHAR(50) NOT NULL,
     precio_actual DOUBLE NOT NULL,
-    tipo ENUM('vegetariana','especial','clasica') NOT NULL
+    tipo ENUM('vegetariana','especial','clasica') NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- Crear tabla unidad_medida
 
 CREATE TABLE unidad_medida (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- Crear tabla ingrediente
 
 CREATE TABLE ingrediente (
-    id INT PRIMARY KEY ,
+    id INT NOT NULL AUTO_INCREMENT ,
     nombre VARCHAR(50) NOT NULL, 
     precio DOUBLE NOT NULL,
     stock INT NOT NULL,
     id_unidad_medida INT NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (id_unidad_medida) REFERENCES unidad_medida(id)
 );
 
 -- Crear tabla ingrediente_pizza
 
 CREATE TABLE ingrediente_pizza (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     cantidad_ingrediente DOUBLE NOT NULL,
     id_pizza INT NOT NULL,
     id_ingrediente INT NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (id_pizza) REFERENCES pizza(id),
     FOREIGN KEY (id_ingrediente) REFERENCES ingrediente(id)
 );
@@ -70,26 +76,29 @@ CREATE TABLE ingrediente_pizza (
 -- Crear tabla pago
 
 CREATE TABLE pago (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     metodo_pago ENUM('efectivo','tarjeta','app') NOT NULL, 
-    estado ENUM('pagado','no pagado') NOT NULL     
+    estado ENUM('pagado','no pagado') NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- Crear tabla zona
 
 CREATE TABLE zona (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     costo_metro DOUBLE NOT NULL,
-    costo_base DOUBLE NOT NULL
+    costo_base DOUBLE NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- Crear tabla repartidor
 
 CREATE TABLE repartidor (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     estado ENUM('disponible','no disponible') NOT NULL,
     id_zona INT NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES persona(id),
     FOREIGN KEY (id_zona) REFERENCES zona(id)
 );
@@ -97,20 +106,21 @@ CREATE TABLE repartidor (
 -- Crear tabla domicilio
 
 CREATE TABLE domicilio (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     distancia DOUBLE NOT NULL,
     hora_salida_repartidor DATETIME,
     hora_entrega DATETIME,
     direccion VARCHAR(50) NOT NULL,
     costo_envio DOUBLE NOT NULL,
     id_zona INT NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (id_zona) REFERENCES zona(id)
 );
 
 -- Crear tabla pedido
 
 CREATE TABLE pedido (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     fecha DATETIME NOT NULL,
     estado ENUM('pendiente','en preparacion','entregado', 'cancelado') NOT NULL,
     total_pedido DOUBLE NOT NULL,
@@ -119,6 +129,7 @@ CREATE TABLE pedido (
     id_domicilio INT NOT NULL,
     id_repartidor INT NOT NULL,
     id_usuario INT NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id),
     FOREIGN KEY (id_pago) REFERENCES pago(id),
     FOREIGN KEY (id_domicilio) REFERENCES domicilio(id),
@@ -129,12 +140,13 @@ CREATE TABLE pedido (
 -- Crear tabla detalle_pedido
 
 CREATE TABLE detalle_pedido (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     precio_unitario DOUBLE NOT NULL,
     cantidad INT NOT NULL,
     subtotal DOUBLE NOT NULL,
     id_pedido INT NOT NULL,
     id_pizza INT NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (id_pedido) REFERENCES pedido(id),
     FOREIGN KEY (id_pizza) REFERENCES pizza(id)
 );
@@ -142,7 +154,8 @@ CREATE TABLE detalle_pedido (
 -- Crear tabla historial_precio
 
 CREATE TABLE historial_precio (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     precio_antiguo DOUBLE NOT NULL,
-    precio_nuevo DOUBLE NOT NULL
+    precio_nuevo DOUBLE NOT NULL,
+    PRIMARY KEY (id)
 );
