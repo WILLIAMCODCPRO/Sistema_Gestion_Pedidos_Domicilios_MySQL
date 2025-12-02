cc39e30fe91d3e306b3175a658acbe5000cbfba6
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-12-02 16:28:03 -0500

    :sparkles: feat(funciones): add total_pedido fuction

---
+++ funciones.sql
@@ -0,0 +1,22 @@
+-- Función para calcular el total de un pedido (sumando precios de pizzas + costo de envío + IVA).
+
+DELIMITER //
+
+CREATE FUNCTION total_pedido(p_id_pedido INT)
+RETURNS DOUBLE
+DETERMINISTIC
+READS SQL DATA
+
+BEGIN
+    DECLARE v_subtotal_pizza DOUBLE;
+    DECLARE v_envio DOUBLE;
+    DECLARE iva  DOUBLE DEFAULT 0.19;
+    DECLARE total DOUBLE;
+
+    SELECT SUM(dp.subtotal) INTO v_subtotal_pizza FROM detalle_pedido dp WHERE dp.id_pedido = p_id_pedido ;
+    SELECT d.costo_envio INTO v_envio FROM pedido p INNER JOIN domicilio d ON d.id = p.id_domicilio WHERE p.id = p_id_pedido ;
+    SET total = (v_subtotal_pizza + v_envio) * (1 + iva);
+    RETURN total;
+END //
+
+DELIMITER ;
