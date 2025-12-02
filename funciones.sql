4fb690bc0a4f4dd51b63a669e63755ba3196016b
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-12-02 18:23:52 -0500

    :sparkles: feat(funciones): add ganancia_neta_diaria fuction

--- funciones.sql
+++ funciones.sql
@@ -20,3 +20,26 @@ BEGIN
 END //
 
 DELIMITER ;
+
+-- Función para calcular la ganancia neta diaria (ventas - costos de ingredientes).
+
+DELIMITER //
+
+CREATE FUNCTION ganancia_neta_diaria(p_fecha DATE)
+RETURNS DOUBLE
+DETERMINISTIC
+READS SQL DATA
+
+BEGIN
+   DECLARE v_ganancia_neta_diaria DOUBLE;
+   DECLARE v_subtotal_pizza DOUBLE;
+   DECLARE v_costos_ingredientes DOUBLE;
+
+   SELECT SUM(dp.subtotal) INTO v_subtotal_pizza FROM detalle_pedido dp INNER JOIN pedido p ON p.id = dp.id_pedido WHERE DATE(p.fecha) = p_fecha AND p.estado = 'entregado';
+   SELECT SUM(dp.cantidad * ip.cantidad_ingrediente * i.precio) INTO v_costos_ingredientes FROM detalle_pedido dp INNER JOIN pedido p ON p.id = dp.id_pedido INNER JOIN ingrediente_pizza ip ON ip.id = dp.id_pizza INNER JOIN ingrediente i ON i.id = ip.id_ingrediente WHERE DATE(p.fecha) = p_fecha AND p.estado = 'entregado';
+   SET v_ganancia_neta_diaria = (v_subtotal_pizza - v_costos_ingredientes);
+   RETURN v_ganancia_neta_diaria;
+
+END //
+
+DELIMITER ;
