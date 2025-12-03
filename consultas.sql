a8141bc0c674424dd6c52337e2a70e17879b49d6
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-12-03 07:41:45 -0500

    :sparkles: feat(consultas): add pediod por repartidor  query

--- consultas.sql
+++ consultas.sql
@@ -1,7 +1,11 @@
 -- Clientes con pedidos entre dos fechas (BETWEEN).
 
-SELECT p.nombre, COUNT(pe.id) AS cantidad_pedidos_reaizados FROM pedido pe INNER JOIN persona p ON pe.id_cliente = p.id WHERE pe.fecha BETWEEN '2025-01-01' AND '2025-01-12' GROUP BY p.nombre ;
+SELECT p.nombre AS cliente, COUNT(pe.id) AS cantidad_pedidos_reaizados FROM pedido pe INNER JOIN persona p ON pe.id_cliente = p.id WHERE pe.fecha BETWEEN '2025-01-01' AND '2025-01-12' GROUP BY p.nombre ;
 
 -- Pizzas más vendidas (GROUP BY y COUNT).
 
 SELECT p.nombre, SUM(dp.cantidad) AS cantidad_vendida FROM detalle_pedido dp INNER JOIN pizza p ON p.id = dp.id_pizza GROUP BY p.nombre ORDER BY cantidad_vendida DESC LIMIT 5;
+
+-- Pedidos por repartidor (JOIN).
+
+SELECT p.nombre AS repartidor, COUNT(pe.id) AS cantidad_pedidos_repartidos FROM pedido pe INNER JOIN persona p ON p.id = pe.id_repartidor GROUP BY p.nombre ORDER BY cantidad_pedidos_repartidos DESC;
