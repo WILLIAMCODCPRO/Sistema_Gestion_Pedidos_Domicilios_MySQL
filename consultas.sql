e4e438684997e0a86203f2d3da9295c566c4b1da
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-12-03 03:24:08 -0500

    :sparkles: feat(consultas): add pizzas mas vendidas query

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,7 @@
 -- Clientes con pedidos entre dos fechas (BETWEEN).
 
 SELECT DISTINCT p.nombre FROM pedido pe INNER JOIN persona p ON pe.id_cliente = p.id WHERE pe.fecha BETWEEN '2025-12-01' AND '2025-12-30';
+
+-- Pizzas más vendidas (GROUP BY y COUNT).
+
+SELECT p.nombre, SUM(dp.cantidad) AS cantidad_vendida FROM detalle_pedido dp INNER JOIN pizza p ON p.id = dp.id_pizza GROUP BY p.nombre ORDER BY cantidad_vendida DESC LIMIT 5;
